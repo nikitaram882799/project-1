5b1420b87ee40d4eb7b64a2097b3111d0d303081
nikithar98 <nikitha.r@everestek.com>
2025-01-09 22:17:22 +0530

    updated infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-project1-tfstate"
     key     = "backend/terraform-project1.tfstate"
-    region  = "us-east-1"
+    region  = "us-west-2"
     profile = "default"
   }
 }
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,5 +1,5 @@
 
-aws_region = "us-east-1"
+aws_region = "us-west-2"
 aws_profile = "default"
 project_name = "Dev-project"
 
