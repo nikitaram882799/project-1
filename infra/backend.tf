23333eddcf8f8444790f9d292941222651a66932
nikithar98 <nikitha.r@everestek.com>
2025-01-09 22:23:31 +0530

    updated backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket  = "terraform-project1-tfstate"
     key     = "backend/terraform-project1.tfstate"
     region  = "us-west-2"
-    profile = "default"
+    profile = "demo"
   }
 }
