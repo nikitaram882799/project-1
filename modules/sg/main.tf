cc87d6a64e5be1d6387faf935888c8866ca3e7c0
nikitaram882799 <90086505+nikitaram882799@users.noreply.github.com>
2025-01-09 13:04:20 +0530

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -37,25 +37,6 @@ resource "aws_security_group" "dev_sg" {
         description = "Allow outbound"
     }
 
-    tags = {
-        Name = "${var.project_name}-sg"
-    }
-}
-
-
-resource "aws_security_group" "ec2_jenkins_port_8080" {
-  description = "Enable the Port 8080 for jenkins"
-  vpc_id      = var.vpc_id
-
-  # ssh for terraform remote exec
-  ingress {
-    description = "Allow 8080 port to access jenkins"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "TCP"
-  }
-
   tags = {
     Name = "Security Groups to allow SSH(22) and HTTP(80)"
   }
