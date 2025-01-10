26a7f8348f977fc0c49a8d7aa65ca66e3d92e0e6
nikithar98 <nikitha.r@everestek.com>
2025-01-10 15:27:52 +0530

    updated script

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -13,6 +13,7 @@ resource "aws_db_instance" "default" {
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
+  db_name                 = var.mysql_dbname
   vpc_security_group_ids  = [var.rds_mysql_sg_id]
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
   skip_final_snapshot     = true
